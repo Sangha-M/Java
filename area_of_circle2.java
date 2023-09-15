9df32cf6407b3620b9c98d52c5113eb4e7e7ae6f
Sangha-M <144378652+Sangha-M@users.noreply.github.com>
2023-09-15 13:57:16 -0400

    Create area_of_circle2.java

---
+++ area_of_circle2.java
@@ -0,0 +1,14 @@
+//Area of a circle
+class Main {
+  public static void main(String[] args) {
+  
+  final double pi=3.141592; 
+  double r=4.5;
+   double answer = pi*(r*r);
+    System.out.println("Area of a circle is "+ answer);
+
+    double x = 7.7;
+    double area = pi*(x*x);
+    System.out.println("Areas of Circle 2 is "+ area);
+  }
+}
