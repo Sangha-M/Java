07b214d101b974c18627b8d4fe8422d016b61b69
Sangha-M <144378652+Sangha-M@users.noreply.github.com>
2023-09-19 13:23:40 -0400

    Relation_opperator.java

---
+++ Relation_opperator.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+double a,b; //intialisation 
+    a=6; //declaration 
+    b=3; 
+  double x = a%b; //modulo operator(remainder)
+  System.out.println(x);
+  double q=a/b;
+  double y=a/b;
+//you need double everywehere to get the decimal point, if not double then it is represented by 2.0 or whaever 
+    System.out.println("intreger value is "+q);
+    System.out.println("Double value is "+y);
+
+  System.out.println(1.9<1.19);//this will give true or false 
+    System.out.println(120!=4);
+  }
+}
