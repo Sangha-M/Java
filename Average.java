7a3c5d46e696f7e3d03f9bdb17b99386732bee57
Sangha-M <144378652+Sangha-M@users.noreply.github.com>
2023-09-21 13:43:21 -0400

    Create Average.java

---
+++ Average.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    //System.out.println((5>3)||(9<4)); // or operator 
+     //System.out.println((5!=120));
+    //String name = "daniel";
+   
+   // System.out.println(name);
+double c=3.45, x=9.29;
+    System.out.println("Average cx = "+(c+x)/2);
+      
+  }
+}
