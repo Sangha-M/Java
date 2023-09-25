0c987f45263f58ade30c6348ed54a30da19504dc
Sangha-M <144378652+Sangha-M@users.noreply.github.com>
2023-09-25 13:33:03 -0400

    Create Opperator_second_stuff_plus_minus.java

---
+++ Opperator_second_stuff_plus_minus.java
@@ -0,0 +1,24 @@
+class Main {
+  public static void main(String[] args) {
+   
+    int u=3;
+      //u=u+10;
+    u+=10;
+    System.out.println(u);
+    
+    double t=14;
+      t=t/2;
+      System.out.println(t);
+    
+    int x=5;
+      x=x*4;
+    System.out.println(x);
+    
+    int z=34;
+      z/=2;
+      System.out.println("ADDITION"+z);
+
+    
+    int 
+  }
+}
