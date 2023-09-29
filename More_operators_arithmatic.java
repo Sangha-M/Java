51045d8bddbc04b1b42399f501f9bf173aee873a
Sangha-M <144378652+Sangha-M@users.noreply.github.com>
2023-09-29 13:29:06 -0400

    Update More_operators_arithmatic.java

--- More_operators_arithmatic.java
+++ More_operators_arithmatic.java
@@ -11,14 +11,19 @@ double a,b; //intialisation
     System.out.println("intreger value is "+q);
     System.out.println("Double value is "+y);
 
+
+    //next are relational opperators
   System.out.println(1.9<1.19);//this will give true or false 
     System.out.println(120!=4);
      System.out.println(120>4);
-        System.out.println(7==3);
-        System.out.println(120!=4);//relational opperators
+        System.out.println(7==3);//== is a comparison opperator whilst = is an assignment 
+        System.out.println(120!=4);
      System.out.println(a>=b);
-        System.out.println(12<=6);
-    System.out.println((5>3) && (8>5)); //logical opperators
+        System.out.println(12<=6);//relational ends 
+    
+
+//next code: logical opperators
+    System.out.println((5>3) && (8>5)); 
      System.out.println((5<3) && (8<5));
   }
 }
