5d144da928de3ed8d3af0eb6275f9de1438eacdb
Sangha-M <144378652+Sangha-M@users.noreply.github.com>
2023-09-29 13:09:18 -0400

    Create More_operators_arithmatic.java

---
+++ More_operators_arithmatic.java
@@ -0,0 +1,24 @@
+class Main {
+  public static void main(String[] args) {
+double a,b; //intialisation 
+    a=6; //declaration 
+    b=3; 
+  double x = a%b; //modulo operator(remainder)
+  System.out.println(x);
+  double q=a/b; // arithmatic opperator 
+  double y=a/b;
+//you need double everywehere to get the decimal point, if not double then it is represented by 2.0 or whaever 
+    System.out.println("intreger value is "+q);
+    System.out.println("Double value is "+y);
+
+  System.out.println(1.9<1.19);//this will give true or false 
+    System.out.println(120!=4);
+     System.out.println(120>4);
+        System.out.println(7==3);
+        System.out.println(120!=4);//relational opperators
+     System.out.println(a>=b);
+        System.out.println(12<=6);
+    System.out.println((5>3) && (8>5)); //logical opperators
+     System.out.println((5<3) && (8<5));
+  }
+}
