a5a48a556f42d227ced55e88b6d47d348343eb7b
Sangha-M <144378652+Sangha-M@users.noreply.github.com>
2023-09-30 19:01:13 -0400

    Create nested_if_with_if_stataemnts.java

---
+++ nested_if.java
@@ -0,0 +1,33 @@
+class Main {
+  public static void main(String[] args) {
+//nested if statement 
+    int x=19; 
+    if(x>30)
+    {
+      System.out.println(x+ " is greater than 30");
+    }
+      if(x>25) 
+      {
+        System.out.println(x+ " is greater than 25");
+      }
+
+        if(x>15) 
+          {
+            System.out.println(x+ " is greater than 15");
+          }
+            if(x>10) 
+          {
+            System.out.println(x+ " is greater than 10");
+          }
+/*
+//if statement 
+double z=3.2
+  if(z>2)
+      {
+        System.out.println(z+" is greater than 2")
+      }
+*/
+    
+  
+  }
+}
