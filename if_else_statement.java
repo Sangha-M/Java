daa080cf020185c729a09cd8c7770c0107580069
Sangha-M <144378652+Sangha-M@users.noreply.github.com>
2023-09-30 18:51:17 -0400

    Create if_else_statement.java

---
+++ if_else_statement.java
@@ -0,0 +1,27 @@
+class Main {
+  public static void main(String[] args) {
+
+    int x=25; 
+    if(x>30)
+    {
+      System.out.println(x+ " Is greater than 30");
+    }
+    
+    else if(x>20)
+      {
+        System.out.println(x+" Is greater than 20");
+
+      
+      }
+
+          else if(x>10)
+            {
+              System.out.println(x+ " is greater than 10");
+            } 
+
+
+    System.out.print("hi"):
+      System.out.println("bye"):
+    
+  }
+}
