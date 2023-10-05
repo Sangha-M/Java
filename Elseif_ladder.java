9cb445ef8666423ee8135ba3e5327067af362c12
Sangha-M <144378652+Sangha-M@users.noreply.github.com>
2023-10-05 13:10:55 -0400

    Create Elseif_ladder.java

---
+++ Elseif_ladder.java
@@ -0,0 +1,38 @@
+class Main {
+  public static void main(String[] args) {
+
+
+//is a number odd or even
+    int x=500; 
+    
+    if(x/3>=90)
+    {
+        System.out.println("grade is A");
+    }
+else if(x/3>=80)
+    {
+        System.out.println("grade is B");
+    }
+    
+else if(x/3>=80)
+  
+    {
+        System.out.println("grade is C");
+    }
+    //beter way to write 
+
+    int per=100;
+      if (per>=95)
+      {
+        System.out.println(" Average is above 95%");
+
+          }
+else if(per>=85)
+  System.out.println("it's okay"); 
+
+        
+    
+    
+    }
+    
+}
