435b509094f9b6838835d76d66b41f9e61f2f341
Sangha-M <144378652+Sangha-M@users.noreply.github.com>
2023-10-10 13:29:48 -0400

    Create in class_work_three_programs.java

---
+++ in class_work_three_programs.java
@@ -0,0 +1,94 @@
+//Program 1
+//Muhammed
+/*
+class Main {
+  public static void main (String[] args) {
+    System.out.println("o000()");
+    System.out.println(" /  _)");
+    System.out.println(" |  (");
+    System.out.println(" \\ _)  ()000o");
+    System.out.println("        (_  \\");
+    System.out.println("         )  |");
+    System.out.println("        (  _/");
+    System.out.println(" o000()");
+    System.out.println(" /  _)");
+    System.out.println(" |   (");
+    System.out.println(" \\ _) ()000o");
+    System.out.println("        (_  \\");
+    System.out.println("         )  |");
+    System.out.println("        (_  /");
+   }
+}
+
+*/
+
+/*number 2
+//Mehtab
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    
+
+      Scanner input=new Scanner(System.in); 
+    System.out.print("Enter a grade ");
+    
+    //takes the input from your keyboard
+    
+   int grade1 = input.nextInt();
+    
+ if(grade1>=80)
+
+ {//prints the message/answer 
+    System.out.println("grade is above 80%");
+ }
+  
+else if(grade1>=60)
+  {
+    System.out.println("Grade is more than 60%");  
+  }
+else if(grade1>=40)
+  {
+    System.out.println("Average");  
+  }
+    
+
+else if(grade1<40)
+  {
+    System.out.println("Fail");  
+  }
+    
+   //closes the scanner
+    input.close();
+  }
+}*/
+
+//PROGRAM 3
+//Gurtaj
+/*
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+final double PI = 3.14159265359; // Define PI as a constant
+        
+        // Create a Scanner object to read user input
+        Scanner scanner = new Scanner(System.in);
+        
+        // Prompt the user to enter the radius
+        System.out.print("Enter the radius: ");
+        double radius = scanner.nextDouble();
+        
+        if (radius > 0) {
+            // Calculate the area of the circle using the formula: Area = PI * radius * radius
+            double area = PI * radius * radius;
+            
+            // Display the calculated area
+            System.out.println("The area of the circle is: " + area);
+        } else {
+            // Display a message for negative input
+            System.out.println("Negative input");
+        }
+        
+        // Close the scanner
+        scanner.close();
+    }
+}*/
