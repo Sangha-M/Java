28baf55abc0c24285006b6e29872265bcd508ada
Sangha-M <144378652+Sangha-M@users.noreply.github.com>
2023-10-14 16:01:43 -0400

    Update take_home_work.java

--- take_home_work.java
+++ take_home_work.java
@@ -204,6 +204,137 @@ if(n.equals("Calgary"))
 }
 */
 
-//Part B 
-//Question 2
+/*//take home assignment part b, number 2
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
 
+
+      Scanner input=new Scanner(System.in); 
+
+    //tels the user to enter their numbers
+    System.out.println("Enter first number ");
+     //takes the input from your keyboard
+    double x = input.nextDouble();
+    System.out.println("Enter second number ");
+     //takes the input from your keyboard
+    double y = input.nextDouble();
+    System.out.println("Enter third number ");
+     //takes the input from your keyboard
+    double z = input.nextDouble();
+    
+   
+//display what the smallest of the three numbers is 
+    if(x<=y && x<=z) //conditional statement 
+
+    {
+      System.out.print("Smallest number is "+x);
+    }
+    else if(y<=x && y<=z)
+    {
+      System.out.println("Smallest number is "+y);
+    }
+    else
+    {
+      System.out.println("Smallest numnber is "+z);
+    }
+   
+
+
+   //closes the scanner
+    input.close();
+  }
+}
+*/
+
+
+
+
+
+//take home assignment part b, number 3
+/*import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+
+
+      Scanner input=new Scanner(System.in); 
+
+    //tels the user to enter their numbers
+    System.out.println("Enter first number ");
+     //takes the input from your keyboard
+    int x = input.nextInt();
+    System.out.println("Enter second number ");
+     //takes the input from your keyboard
+    int y = input.nextInt();
+    System.out.println("Enter third number ");
+     //takes the input from your keyboard
+    int z = input.nextInt();
+    
+    //conditional statement 
+//display what the largest of the three numbers is 
+    if(x>=y && x>=z)
+
+    {
+      System.out.print("Largest number is "+x);
+    }
+    else if(y>=x && y>= z)
+    {
+      System.out.println("largest number is "+y);
+    }
+    else
+    {
+      System.out.println("Largest numnber is "+z);
+    }
+   
+
+
+   //closes the scanner
+    input.close();
+  }
+}*/
+
+
+/*//take home assignment part b, number 4
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+
+
+      Scanner input=new Scanner(System.in); 
+
+    //tels the user to enter their age
+    System.out.println("Enter your age?");
+    
+    int x = input.nextInt(); //takes the input from their keyboard
+
+ 
+    
+   
+//display what the smallest of the three numbers is 
+    
+    if(x<=12){ //conditional statement 
+    //tels user if they are a child, teen, adult, or senior citizen
+   System.out.print("You are a child");
+    }
+   
+    else if(x>=13 && x<=19){
+    
+    System.out.println("You are a teenager");
+    }
+    
+    else if(x>=20 && x<=64){
+    
+     System.out.println("You are an adult");
+    }
+    else if(x>=65){
+    
+    System.out.println("You are a senior citizen");
+    }
+   
+
+
+   //closes the scanner
+    input.close();
+  }
+}
+*/
