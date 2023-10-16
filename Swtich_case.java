f67b8c3c87a89da125ae2b7a510b126e7d713bb0
Sangha-M <144378652+Sangha-M@users.noreply.github.com>
2023-10-16 14:00:02 -0400

    Swtich_case.java

---
+++ Swtich_case.java
@@ -0,0 +1,28 @@
+class Main {
+  public static void main(String[] args) {
+    
+    int x =3;
+     /* if(x>80)
+    {
+      System.out.println("Yes");
+    }
+    if(x>70)
+    {
+      System.out.print("No");
+    */
+  switch(x) 
+  {
+    case 1:
+      System.out.println("90");
+      break;
+    case 2:
+      System.out.println("80");
+      break;
+
+      default:
+      System.out.println("case is other than 1 or 2");
+      break;
+  }
+    
+  }
+}
